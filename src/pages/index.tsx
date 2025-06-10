ae21cbc873ae01865d033d3ba8c828005d38c84b
hnascx <henrique.nascimento12@hotmail.com>
2025-06-10 10:59:04 -0300

    chore: Config shadcn-ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
@@ -15,7 +16,7 @@ export default function Home() {
     <div
       className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
-      <h2>Let's get it on.</h2>
+      <Button>Button</Button>
     </div>
   );
 }
