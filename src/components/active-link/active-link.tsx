c96b351daebea14b208471c5267e22fefd24d759
hnascx <henrique.nascimento12@hotmail.com>
2025-07-02 17:14:17 -0300

    feat: Add style guide

--- src/components/active-link/active-link.tsx
+++ src/components/active-link/active-link.tsx
@@ -8,13 +8,15 @@ type ActiveLinkProps = {
 
 export const ActiveLink = ({ children, href, ...rest }: ActiveLinkProps) => {
   const router = useRouter()
-  const isCurrentPath = 
-    router.asPath === href ||
-    router.asPath === rest.as ||
-    router.asPath.startsWith(String(rest.as))
-  
+  const isCurrentPath = router.asPath === href || router.asPath === rest.as
+
   return (
-    <Link href={href} className={cn("text-sm font-medium transition-colors hover:text-blue-500", isCurrentPath ? 'text-blue-500' : 'text-muted-foreground')}
+    <Link
+      href={href}
+      className={cn(
+        "text-sm transition-colors hover:text-blue-200",
+        isCurrentPath ? "text-blue-200" : "text-gray-100"
+      )}
     >
       {children}
     </Link>
