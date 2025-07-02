34e2966654c9339074db96977c461ffd984e18db
hnascx <henrique.nascimento12@hotmail.com>
2025-07-02 18:25:19 -0300

    feat: Create hero section

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 import { ActiveLink } from "../active-link"
-import { Button } from "../ui/button"
 import { Logo } from "../logo"
+import { Button } from "../ui/button"
 
 export const Header = () => {
   return (
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -14,7 +14,7 @@ export const Layout = ({ children }: LayoutProps) => {
       className={`${inter.className} relative flex min-h-screen flex-col bg-gray-700`}
     >
       <Header />
-      <main className="flex-1 flex flex-col mb-12">{children}</main>
+      <main className="flex-1 flex flex-col mt-10 mb-12">{children}</main>
       <Footer />
     </div>
   )
