33ac6e83cadcf211ae8a915898626114741129da
hnascx <henrique.nascimento12@hotmail.com>
2025-07-03 17:20:13 -0300

    chore: Setup content layer

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
+import { withContentlayer } from "next-contentlayer"
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   devIndicators: false,
-};
+}
 
-export default nextConfig;
+export default withContentlayer(nextConfig)
