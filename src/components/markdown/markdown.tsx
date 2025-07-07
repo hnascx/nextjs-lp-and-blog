9b517622d8fbbff67d5833bf38ebd4e087b6ffac
hnascx <henrique.nascimento12@hotmail.com>
2025-07-07 12:47:03 -0300

    feat: Create markdown component

---
+++ src/components/markdown/markdown.tsx
@@ -0,0 +1,44 @@
+import ReactMarkdown from "react-markdown"
+import remarkGfm from "remark-gfm"
+
+type MarkdownProps = {
+  content: string
+}
+
+export const Markdown = ({ content }: MarkdownProps) => {
+  return (
+    <ReactMarkdown
+      remarkPlugins={[remarkGfm]}
+      components={{
+        h1: ({ children }) => (
+          <h1 className="mb-4 text-heading-md md:text-heading-xl">
+            {children}
+          </h1>
+        ),
+        h2: ({ children }) => (
+          <h2 className="mb-4 mt-8 text-heading-sm md:text-heading-lg">
+            {children}
+          </h2>
+        ),
+        a: ({ children, href }) => (
+          <a
+            href={href}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-200 hover:underline"
+          >
+            {children}
+          </a>
+        ),
+        p: ({ children }) => (
+          <p className="mb-6 leading-relaxed text-gray-200">{children}</p>
+        ),
+        strong: ({ children }) => (
+          <strong className="font-extrabold text-gray-100">{children}</strong>
+        ),
+      }}
+    >
+      {content}
+    </ReactMarkdown>
+  )
+}
