51ff0787ed8c1d1f030fef20ee3caa8332983015
hnascx <henrique.nascimento12@hotmail.com>
2025-07-07 16:26:16 -0300

    feat: Create structure of useShare hook

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -88,11 +88,20 @@ export default function PostPage() {
 
           <aside className="space-y-6">
             <div className="rounded-lg bg-gray-700 p-4 md:p-6 ">
-              <h2 className="mb-4 text-heading-xs text-gray-100">Compartilhar</h2>
+              <h2 className="mb-4 text-heading-xs text-gray-100">
+                Compartilhar
+              </h2>
 
               <div className="space-y-3 ">
-                {[{ key: '1', providerName: 'LinkedIn' }].map((provider) => (
-                  <Button key={provider.key} variant="outline">
+                {[
+                  { key: "1", providerName: "LinkedIn" },
+                  { key: "2", providerName: "Facebook" },
+                ].map((provider) => (
+                  <Button
+                    key={provider.key}
+                    variant="outline"
+                    className="w-full justify-start gap-2"
+                  >
                     {provider.providerName}
                   </Button>
                 ))}
