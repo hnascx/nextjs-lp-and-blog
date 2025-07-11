2955d97d426cda8fb2579d86c9bec5703855a712
hnascx <henrique.nascimento12@hotmail.com>
2025-07-11 12:51:44 -0300

    feat: Add dynamic metadata to post page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next"
 import { notFound } from "next/navigation"
 import { allPosts } from "../../../../.contentlayer/generated"
 import { PostPage } from "../../../templates/blog"
@@ -6,6 +7,27 @@ type BlogPostPageProps = {
   params: Promise<{
     slug: string
   }>
+}
+
+export async function generateMetadata({
+  params,
+}: BlogPostPageProps): Promise<Metadata> {
+  const { slug } = await params
+  const post = allPosts.find((post) => post.slug === slug)
+
+  if (!post) {
+    return {}
+  }
+
+  return {
+    title: post.title,
+    description: post.description,
+    authors: [{ name: post.author.name }],
+    robots: "index, follow",
+    openGraph: {
+      images: [post.image],
+    },
+  }
 }
 
 export const revalidate = 60
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,22 @@ import { Layout } from "../components/layout"
 export const metadata: Metadata = {
   title: "Site.Set",
   description: "Venda seus produtos como afiliado em um único lugar",
+  openGraph: {
+    title: "Site.Set",
+    description: "Venda seus produtos como afiliado em um único lugar",
+    url: "https://nextjs-lp-and-blog.vercel.app/og-image.jpg",
+    siteName: "Site.Set",
+    locale: "pt_BR",
+    type: "website",
+    images: [
+      {
+        url: "https://nextjs-lp-and-blog.vercel.app/og-image.jpg",
+        width: 800,
+        height: 600,
+        alt: "Site.Set",
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
